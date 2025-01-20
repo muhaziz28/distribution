9beb5173f0fa44ca1fefd092c7bef473b9f7e0b9
haikal <muhammadhaikalaziz28@gmail.com>
2025-01-20 03:30:44 +0000

    fix update server

--- resources/views/tukang/index.blade.php
+++ resources/views/tukang/index.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Tukang</h1>
+                    <h1 class="m-0">Tukaang</h1>
                 </div>
                 <div class="col-sm-6">
 
